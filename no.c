75e5212cceafc7ff56838335745a5c2bbf940928
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2017-09-10 15:47:13 +0530

    Create no.c

---
+++ no.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number",n);
+if(n==0)
+{
+printf("number is zero");
+}
+elseif(n>0)
+{
+printf("number is positive");
+}
+else
+{
+printf("number is negative");
+}
+}
