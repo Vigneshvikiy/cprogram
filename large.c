e6268af45a4cf2134f70dead2fbe4c1d0be855c4
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2017-09-11 19:00:19 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter 3 numbers",a,b,c);
+if(a>b&&a>c)
+{
+printf("a is largest");
+}
+elseif(b>a&&b>c)
+{
+printf("b is largest");
+}
+else
+{
+printf("c is largest");
+}
+}
