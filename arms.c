6155a4dfb13e3c2ac8f757d2f990b2a2807fd878
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2017-09-16 19:29:09 +0530

    Create arms.c

---
+++ arms.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+intn,d,result=0,t;
+printf("enter the number",n);
+n=t;
+while(n!=0)
+{
+d=n%10;
+result=result+(d*d*d);
+n=n/10;
+}
+if(t==result)
+{
+printf("the number is armstrong");
+}
+else
+{
+printf("the number is not armstrong");
+}
+}
