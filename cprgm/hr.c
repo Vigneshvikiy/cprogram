fd58ca75db2bf6205abdabe22ed875d6bd722782
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2018-01-20 19:10:16 +0530

    Create hr.c

---
+++ cprgm/hr.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int h1,m1,h2,m2,hr,mr;
+printf("enter the first term:");
+scanf("%d\t%d",&h1,&m1);
+printf("enter the second term:");
+scanf("%d\t%d",&h2,&m2);
+hr=h1-h2;
+mr=m1-m2;
+printf("result is:%d\t%d",hr,mr);
+return 0;
+}
