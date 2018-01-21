25f36c59d557f5efc9f8a39d253de438254b990f
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2018-01-21 11:33:57 +0530

    Create strlen.c

---
+++ cprgm/strlen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+	char a[20],i;
+	printf("enter the string:");
+	scanf("%s\n",&a);
+	for(i=0;a[i]!='\0';++i);
+	{
+		printf("total character is %d",i);
+	}
+	return 0;
+}
