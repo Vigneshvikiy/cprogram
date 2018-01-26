a8ff010467db5df24659ec4035d0f4917ab682ff
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2018-01-26 19:54:52 +0530

    Create numeric.c

---
+++ cprgm/numeric.c
@@ -0,0 +1,13 @@
+#include<stdio.h?
+int main()
+{
+char a[50];
+int len,i,number=0;
+printf("enter the string:");
+scanf("%[\n]d",&a);
+len=strlen(a);
+for(i=0;i<len;i++)
+{
+if(a>=0&&a<=9)
+number=number+1;
+}
