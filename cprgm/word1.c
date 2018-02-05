228db28af049de019f97c3f451f0fa9dce2a364a
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2018-02-05 16:08:23 +0530

    Create word1.c

---
+++ cprgm/word1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[100];
+	int len,i,word=1;
+	printf("enter the string:\n");
+	gets(a);
+	len=strlen(a);
+	for(i=0;i<len;i++)
+	{
+		if(a[i]!='.'&&a[i+1]==' ')
+		{
+			word=word+1;
+		}
+	}
+	printf("there are %d words",word);
+	return 0;
+}
