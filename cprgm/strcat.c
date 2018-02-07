ce33180829fc164019e0843b7faedc475a8b22b3
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2018-02-07 15:42:11 +0530

    Create strcat.c

---
+++ cprgm/strcat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main(void) {
+	char a[10],b[10];
+	printf("enter the string:\n");
+	scanf("%s",&a);
+	printf("enter the second string:\n");
+	scanf("%s",&b);
+	printf("%s%s",a,b);
+	return 0;
+}
