8e3ffae940c3026d268201451cbd78b81fa54c0d
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2018-02-07 19:29:02 +0530

    Update arrmed.c

--- arrmed.c
+++ arrmed.c
@@ -2,7 +2,7 @@
 int main()
 {
 int a[50]={1,3,2,4,5,6,7,9,87,12};
-int max,i,n;
+int max,i;
 max=a[0];
 for(i=1;i<=10;i++)
 {
@@ -11,7 +11,6 @@ if(a[i]>max)
 max=a[i];
 }
 }
-int n=sizeof(a)/sizeof(int);
-printf("%d",a[n/2]);
+printf("%d",ma);
 return 0;
 }
