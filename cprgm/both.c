45c0f521bee2cbf70030b18f1ed9f4c5a9de5488
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2018-02-12 16:57:01 +0530

    Create both.c

---
+++ cprgm/both.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    char a[100];
+    int i,count=0,count1=0;
+    printf("Enter the string:");
+    scanf("%s",&a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]>'a'&&a[i]<'z'||a[i]>'A'&&a[i]<'Z')
+        {
+            count++;
+        }
+        else if(a[i]>'0'&&a[i]<'9')
+        {
+            count1++;
+        }
+    }
+    if(count>0&&count1>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
