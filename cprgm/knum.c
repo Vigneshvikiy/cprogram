ad8bc4635a97c8cdad9d18be6b58850ed20ed7f6
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2018-02-19 15:40:59 +0530

    Update knum.c

--- cprgm/knum.c
+++ cprgm/knum.c
@@ -6,7 +6,7 @@ int main()
     scanf("%d %d",&n,&k);
     for(i=0;i<n;i++)
     {
-        scanf("%d",a[i]);
+        scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
     {
@@ -16,7 +16,7 @@ int main()
     {
         if(a[i]==k)
         {
-            printf("%d",a[i]);
+            printf("\n%d",a[i]);
         }
     }
     return 0;
