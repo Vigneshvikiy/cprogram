ec873e3f7bb085cffb76dfc8417920600caed9cb
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2018-02-21 16:48:20 +0530

    Create multivalue.c

---
+++ cprgm/multivalue.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    scanf("%d %d",&a,&b);
+    printf("\n%d / %d",a,b);
+    scanf("%d %d",&c,&d);
+    printf("\n%d %\t%d",c,d);
+    scanf("%d %d",&e,&f);
+    printf("\n%d / %d",e,f);
+    printf("\n%d",(a/b));
+    printf("\n%d",(c%d));
+    printf("\n%d",(e/f));
+    return 0;
+}
