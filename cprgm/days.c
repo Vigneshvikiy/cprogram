3b74d897250f7db3751b924c58c3a54883239ad7
krishnasubhashini <31506086+krishnasubhashini@users.noreply.github.com>
2018-03-14 16:17:14 +0530

    Update days.c

--- cprgm/days.c
+++ cprgm/days.c
@@ -4,7 +4,7 @@ int main()
     char a[10];
     printf("enter the day:");
     scanf("%d",&a);
-    if(a[10]=='monday'||a[10]=='tuesday'||a[10]=='wednesday'||a[10]=='thursday'||a[10]=='friday')
+    if(a[10]=="monday"||a[10]=="tuesday"||a[10]=="wednesday"||a[10]=="thursday"||a[10]=="friday")
     {
         printf("no");
     }
